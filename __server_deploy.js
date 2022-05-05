e68ac9b507113d95754b897368826798a06577f8
tanjid111 <tanjidhossain@gmail.com>
2022-05-04 22:41:38 -0700

    fixed

--- __server_deploy.js
+++ __server_deploy.js
@@ -12,8 +12,20 @@ For each project one time
 -------------------------------
 1. heroku create
 2. make sure you: git add . git commit git push
-3.  git push heroku main
+3. git push heroku main
 4. go to heroku dashboard > current project > Settings > Reveal config vars
 5. copy paste config vars from your .env file.
 6. Make sure you have whitelisted all ip address to access mongodb
+-------------------------------
+Update Server
+-------------------------------
+1. Make changes
+2. make sure you: git add . git commit git push
+3. git push heroku main
+-------------------------------
+Connect server with Client and deploy client
+-------------------------------
+1. replace localhost by heroku link
+2. npm run build
+3. firebase deploy
 */
--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
 
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId }
+            const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query)
             res.send(service)
         })
